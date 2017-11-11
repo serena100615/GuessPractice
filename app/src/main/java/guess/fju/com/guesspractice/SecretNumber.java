1d5baaa0b672a9bcd30e4841891aba265200b60d
serena100615 <serena100615@gmail.com>
2017-11-11 12:44:19 +0800

    Initial commit

---
+++ app/src/main/java/guess/fju/com/guesspractice/SecretNumber.java
@@ -0,0 +1,38 @@
+package guess.fju.com.guesspractice;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class SecretNumber extends AppCompatActivity {
+    Random ran = new Random();
+    int n = ran.nextInt(100)+1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secret_number);
+    }
+    public void guess(View view){
+        TextView txt_front = (TextView) findViewById(R.id.txt_front);
+        TextView txt_back = (TextView) findViewById(R.id.txt_back);
+        EditText ed_enter = (EditText) findViewById(R.id.ed_enter);
+        int number = Integer.parseInt(ed_enter.getText().toString());
+        if (number<n){
+            txt_front.setText(Integer.toString(number));
+        }else if (number>n){
+            txt_back.setText(Integer.toString(number));
+        }else {
+            new AlertDialog.Builder(this)
+                    .setMessage("Boom")
+                    .setPositiveButton("OK",null)
+                    .show();
+        }
+
+
+    }
+}
